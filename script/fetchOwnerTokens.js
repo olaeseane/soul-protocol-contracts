d7e010ec26c2dd7cd35d3ce671bc7057d6e892de
olaeseane <olaesean@gmail.com>
2022-12-03 15:18:01 +0300

    Update scripts

--- script/fetchOwnerTokens.js
+++ script/fetchOwnerTokens.js
@@ -9,17 +9,18 @@ const ABI = [
   'function totalSupply() view returns(uint)',
   'function balanceOf(address) view returns(uint)',
   'function fetchSenderTokens(address _sender) view returns (uint256[])',
+  'function fetchOwnerTokens(address _owner) view returns (uint256[] memory)',
 ];
 
 // const contractAddr = '0x8F4a7f404d09Fea7A9A6c1c9E99a9F341D3DD9A6';
 const contractAddr = '0xd971A8147314118bc930cA88E729F1760e1a938b';
 const contract = new ethers.Contract(contractAddr, ABI, provider);
 
-const senderAddr = '0x34C064b128237DB2B917962c45083Ef140564bD8';
+const ownerAddr = '0x5f6939026c7944A8ca09752039AD30F34c2B7baA';
 
 const main = async () => {
   const totalSupply = await contract.totalSupply();
-  const tokens = await contract.fetchSenderTokens(senderAddr);
+  const tokens = await contract.fetchOwnerTokens(ownerAddr);
   console.log('totalSupply ->', totalSupply);
   console.log('tokens ->', tokens);
 };
